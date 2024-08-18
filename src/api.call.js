a1e7cbfadce72f4e6161fc90a8bd3b40c809ad26
codeAdi2307 <aditya07kl@gmail.com>
2024-08-18 14:24:11 +0530

    new changes

--- src/api.call.js
+++ src/api.call.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:8000/api';
-const API_URL = 'https://crud-task-m6dq.onrender.com';
+const API_URL = 'https://crud-task-m6dq.onrender.com/api';
 
 export const signUp = async (data) => {
   try {
